0c7e9c220e04f78650e91785e9ec88382abf58df
Tianyi Kao (Max) <59313635+Vinskao@users.noreply.github.com>
2023-07-14 15:24:47 +0000

    Initial commit

---
+++ Notes-App/app.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+//fs.writeFileSync('notes.txt','my name is mkao!')
+
+fs.appendFileSync('notes.txt','I live in TP');
+
+
+// challege: append a message to notes.txt
+// use fs.appendFile
